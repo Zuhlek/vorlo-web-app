4b747f4756cf0871c7946162707847712b69beeb
Zuhlek <tristan_kindle@gmx.com>
2023-03-28 21:42:40 +0200

    file upload
    finalized the function

--- src/components/FileUpload.vue
+++ src/components/FileUpload.vue
@@ -4,36 +4,43 @@
 */
 
 <template>
-    <v-file-input 
-        label="Upload template" 
-        variant="solo" 
+    <v-file-input label="Upload template" variant="solo"
         accept=".doc,.docx,.xml,application/msword,application/vnd.openxmlformats-officedocument.wordprocessingml.document"
-        show-size
-        @change="onFileSelected"
-        >
+        show-size @change="onFileSelected">
     </v-file-input>
     <v-btn variant="tonal" @click="onUpload">Upload</v-btn>
+    <br><br>
+
 </template>
 
 <script>
-    import axios from 'axios'
-    export default {
-        name: 'file-upload',
-        data () {
-            return {
-                selectedFile: null
-            }
+
+import axios from 'axios'
+export default {
+    name: 'file-upload',
+    data() {
+        return {
+            selectedFile: null
+        }
+    },
+    methods: {
+        onFileSelected(event) {
+            console.log(event)
+            this.selectedFile = event.target.files[0] //currently only one template at a time, so [0] is ok
         },
-        methods: {
-            onFileSelected(event){
-                console.log(event)
-                this.selectedFile = event.target.files[0] //currently only one template at a time, so [0] is ok
-            },
-            onUpload(){
-                const formData = new FormData();
-                formData.append('file', this.selectedFile);
-                axios.post('http://localhost:8080/upload/template', formData)
-            }
+        async onUpload() {
+            const formData = new FormData();
+            formData.append('file', this.selectedFile, this.selectedFile.name);
+             axios.post('http://localhost:8080/template/upload', formData)
+            //  , {
+            //     headers: {
+            //         "Content-Type": "multipart/form-data",
+            //     }
+            // })
+            .then((res) => {
+                console.log(res)
+            })
         }
     }
+}
 </script>
